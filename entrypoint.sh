16e673c9c5eaaa0661723459598a72c66a98298b
Chad Reesey <reesey275@gmail.com>
2017-08-20 23:47:16 -0400

    Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Set Configuration for Teamspeak in ts3server.ini
 # The following Lines will set the ts3server.ini
 
-cat > ${TS3_DATA}/ts3server.ini <<EOF
+cat > ts3server.ini <<EOF
 logquerycommands=${LOG_QUERY_COMMANDS:-0}
 machine_id=${MACHINE_ID:-}
 default_voice_port=${DEFAULT_VOICE_PORT:-9987}
@@ -19,19 +19,9 @@ EOF
 
 # This checks if it should run with an external MariaDB
 # or SQL_LITE
-if [[ -z ${TS3_MARIADB_DB} ]]; then
+if [[ -z "${TS3_MARIADB_DB}" ]]; then
 
-cat <<EOF >> ${TS3_DATA}/ts3server.ini
-dbplugin=ts3db_mariadb
-dbpluginparameter=${TS3_DATA}/ts3db_mariadb.ini
-dbsqlpath=sql/
-dbsqlcreatepath=create_mariadb
-
-EOF
-
-else
-
-cat <<EOF>> ${TS3_DATA}/ts3server.ini
+cat <<EOF >> ts3server.ini
 dbplugin=ts3db_sqlite3
 dbpluginparameter=
 dbsqlpath=sql/
@@ -39,21 +29,30 @@ dbsqlcreatepath=create_sqlite/
 dbconnections=10
 EOF
 
+else
+
+cat <<EOF >> ts3server.ini
+dbplugin=ts3db_mariadb
+dbpluginparameter=ts3db_mariadb.ini
+dbsqlpath=sql/
+dbsqlcreatepath=create_mariadb
+EOF
+
 # Begin ts3db_mariadb.ini
 # This writes the database settings for MariaDB
-cat < ${TS3_DATA}/ts3db_mariadb.ini <<EOF
+cat > ts3db_mariadb.ini <<EOF
 [config]
-host=${TS3_MARIADB_HOST}
-port={$TS3_MARIADB_PORT}
-username={$TS3_MARIADB_USER}
-password={$TS3_MARIADB_PASS}
-database={$TS3_MARIADB_DB}
+host=$TS3_MARIADB_HOST
+port=$TS3_MARIADB_PORT
+username=$TS3_MARIADB_USER
+password=$TS3_MARIADB_PASS
+database=$TS3_MARIADB_DB
 socket=
 EOF
 # end ts3db_mariadb.ini
 fi
 
-cat <<EOF>> ${TS3_DATA}/ts3server.ini
+cat >> ts3server.ini <<EOF
 logpath=${LOG_PATH:-logs}
 logquerycommands=${LOG_QUERY_COMMAND:-0}
 dbclientkeepdays=${DB_CLIENT_KEEP_DAYS:-30}
@@ -64,4 +63,4 @@ EOF
 # End ts3server.ini
 
 # Run Teamspeak server
-exec ./ts3server_minimal_runscript.sh inifile=${TS3_DATA}/ts3server.ini
+exec ./ts3server_minimal_runscript.sh inifile=files/ts3server.ini
